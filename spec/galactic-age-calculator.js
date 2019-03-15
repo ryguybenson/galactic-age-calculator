de79d7243c46020543531800d3b6c49246bc0c40
Riley Benson <ryguybenson@yahoo.com>
2019-03-15 10:22:27 -0700

    create planet age functions

--- spec/galactic-age-calculator.js
+++ spec/galactic-age-calculator.js
@@ -1,17 +1,20 @@
+import {Date} from './../src/age-calculator.js';
+
+describe('Date', function() {
 
 
+  it('will test if the users age is correct', function() {
 
+  }
+  it('will test if the users age on other planets is correct', function() {
 
+  }
+  it('will test if the users time to live is correct', function() {
 
-it('will test if the users age is correct', function() {
+  }
+  it('will test if the users time passed expected life is correct', function() {
 
-}
-it('will test if the users age on other planets is correct', function() {
+  }
 
-}
-it('will test if the users time to live is correct', function() {
-
-}
-it('will test if the users time passed expected life is correct', function() {
-
-}
+  
+})
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,1 +1,27 @@
-
+export class Age {
+  constructor(month, day, year) {
+    this.date = new Date();
+    this.birth = new Date(month, day, year);
+    this.earth = this.date - this.birth;
+  }
+  venusAge() {
+    const mercuryRatio = 0.24;
+    const mercury = 
+    return mercury;
+  }
+  jupiterAge() {
+    const mercuryRatio = 0.24;
+    const mercury =
+    return mercury;
+  }
+  mercuryAge() {
+    const mercuryRatio = 0.24;
+    const mercury =
+    return mercury;
+  }
+  marsAge() {
+    const mercuryRatio = 0.24;
+    const mercury =
+    return mercury;
+  }
+}
