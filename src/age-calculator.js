99e5d5cb52636a60dc843cc314c6e3436f175a8a
Riley Benson <ryguybenson@yahoo.com>
2019-03-15 10:48:59 -0700

    calculate earth age

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -3,14 +3,23 @@ export class Age {
     this.date = new Date();
     this.birth = new Date(month, day, year);
     this.earth = this.date - this.birth;
+    this.venusAge = venusAge();
+    this.jupiterAge = jupiterAge();
+    this.mercuryAge = mercuryAge();
+    this.marsAge = marsAge();
+  }
+  difference() {
+    const difference = this.date - this.birth;
+    const dayDifference = Math.floor(difference / 31557600000);
+    const totalDifference = Math.floor(dayDifference / 365);
   }
   venusAge() {
-    const mercuryRatio = 0.24;
-    const mercury = 
+    const mercuryRatio = 0.62;
+    const mercury =
     return mercury;
   }
   jupiterAge() {
-    const mercuryRatio = 0.24;
+    const mercuryRatio = 11.86;
     const mercury =
     return mercury;
   }
@@ -20,7 +29,7 @@ export class Age {
     return mercury;
   }
   marsAge() {
-    const mercuryRatio = 0.24;
+    const mercuryRatio = 1.88;
     const mercury =
     return mercury;
   }
